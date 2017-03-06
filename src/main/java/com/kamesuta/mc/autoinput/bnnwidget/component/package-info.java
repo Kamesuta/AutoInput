927c865779af7e6c57f55708b3eae0b088e86660
Komei_000 <Komei_000@PC>
2017-03-06 18:49:27 +0900

    イヒヒヒヒｗｗｗｗｗｗ

---
+++ src/main/java/com/kamesuta/mc/autoinput/bnnwidget/component/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * BnnWidgetの実用的なコンポーネントが含まれるサブセットです
+ */
+@com.kamesuta.mc.autoinput.NonNullByDefault
+package com.kamesuta.mc.autoinput.bnnwidget.component;
---
+++ src/main/java/com/kamesuta/mc/autoinput/bnnwidget/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * BnnWidgetの主要なコンポーネントが含まれるパッケージです。
+ * <p>
+ * {@link com.kamesuta.mc.autoinput.bnnwidget.WFrame WFrame}はMinecraftのGUIとウィジェットをつなぐ重量コンポーネントです。
+ * <br>
+ * {@link com.kamesuta.mc.autoinput.bnnwidget.WCommon WCommon}はウィジェットの軽量コンポーネントの基本インターフェイスです。
+ */
+@com.kamesuta.mc.autoinput.NonNullByDefault
+@API(apiVersion = "1.0", owner = "bnnwidget", provides = "bnnwidget")
+package com.kamesuta.mc.autoinput.bnnwidget;
+
+import cpw.mods.fml.common.API;
