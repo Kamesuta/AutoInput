f40c9710d21a26590f6694297b53071c18c8ee06
sjcl <fruit@bebehp.com>
2017-11-16 02:05:35 +0900

    Fix hold input not working after closing Gui

--- src/main/java/net/teamfruit/autoinput/ClientHandler.java
+++ src/main/java/net/teamfruit/autoinput/ClientHandler.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 
 import com.google.common.collect.ImmutableList;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.gui.GuiDisconnected;
 import net.minecraft.client.gui.GuiDownloadTerrain;
@@ -19,6 +20,7 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.teamfruit.autoinput.gui.GuiAutoInput;
 
 public class ClientHandler {
 	public static final ClientHandler INSTANCE = new ClientHandler();
@@ -38,17 +40,36 @@ public class ClientHandler {
 	}
 
 	public final HashSet<Integer> tickKeys = new HashSet<Integer>();
+	private GuiScreen currentgui;
+	private boolean prev;
 
 	@SubscribeEvent
 	public void onClientTick(final TickEvent.ClientTickEvent event) {
 		if (event.phase==TickEvent.Phase.START&&InputHandler.INSTANCE.isKeyInput())
 			for (final Integer line : this.tickKeys)
 				KeyBinding.onTick(line);
+
+		if (this.prev&&Minecraft.getMinecraft().currentScreen==null) {
+			onGuiClose();
+			this.currentgui = null;
+		}
+		this.prev = this.currentgui!=null;
 	}
 
 	@SubscribeEvent
-	public void onOpenGui(final GuiOpenEvent event) {
-		if (event.getGui()!=null&&!EGNORE_CLASSES.contains(event.getGui().getClass()))
-			InputHandler.INSTANCE.setKeyInput(false);
+	public void onGuiOpen(final GuiOpenEvent event) {
+		if (event.getGui()!=null)
+			if (EGNORE_CLASSES.contains(event.getGui().getClass()))
+				this.currentgui = Minecraft.getMinecraft().currentScreen;
+			else
+				InputHandler.INSTANCE.setKeyInput(false);
 	}
+
+	public void onGuiClose() {
+		if (InputHandler.INSTANCE.isKeyInput())
+			for (final AutoInputKey binding : GuiAutoInput.keys)
+				if (!binding.getMode())
+					KeyBinding.setKeyBindState(binding.getKeyCode(), true);
+	}
+
 }
