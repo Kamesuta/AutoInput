6d6dfc241abbde6fa3ac7e5d68c620cfe458091c
sjcl <fruit@bebehp.com>
2017-11-18 04:32:08 +0900

    #

--- src/main/java/net/teamfruit/autoinput/ClientHandler.java
+++ src/main/java/net/teamfruit/autoinput/ClientHandler.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 
 import com.google.common.collect.ImmutableList;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.gui.GuiDisconnected;
 import net.minecraft.client.gui.GuiDownloadTerrain;
@@ -39,8 +38,6 @@ public class ClientHandler {
 	}
 
 	public final HashSet<Integer> tickKeys = new HashSet<Integer>();
-	private GuiScreen currentgui;
-	private boolean prev;
 
 	@SubscribeEvent
 	public void onClientTick(final TickEvent.ClientTickEvent event) {
@@ -48,27 +45,18 @@ public class ClientHandler {
 			for (final Integer line : this.tickKeys)
 				KeyBinding.onTick(line);
 
-		if (this.prev&&Minecraft.getMinecraft().currentScreen==null) {
-			onGuiClose();
-			this.currentgui = null;
-		}
-		this.prev = this.currentgui!=null;
 	}
 
 	@SubscribeEvent
 	public void onGuiOpen(final GuiOpenEvent event) {
 		if (event.getGui()!=null)
-			if (EGNORE_CLASSES.contains(event.getGui().getClass()))
-				this.currentgui = Minecraft.getMinecraft().currentScreen;
-			else
+			if (EGNORE_CLASSES.contains(event.getGui().getClass())) {
+				if (InputHandler.INSTANCE.isKeyInput())
+					for (final AutoInputKey binding : KeyStore.INSTANCE.getKeys())
+						if (binding.getKeyCode()!=0&&!binding.getMode())
+							KeyBinding.setKeyBindState(binding.getKeyCode(), true);
+			} else
 				InputHandler.INSTANCE.setKeyInput(false);
-	}
-
-	public void onGuiClose() {
-		if (InputHandler.INSTANCE.isKeyInput())
-			for (final AutoInputKey binding : KeyStore.INSTANCE.getKeys())
-				if (!binding.getMode())
-					KeyBinding.setKeyBindState(binding.getKeyCode(), true);
 	}
 
 }
